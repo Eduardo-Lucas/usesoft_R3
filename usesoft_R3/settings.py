faf22fd48ac55bec61dc5f462aa2a13be597a885
Eduardo Lucas <eduardolucas40@gmail.com>
2018-07-23 20:39:13 -0300

    Final Adjustments

--- usesoft_R3/settings.py
+++ usesoft_R3/settings.py
@@ -139,13 +139,16 @@ DATABASES = {
         # 'ENGINE': 'tenant_schemas.postgresql_backend',
 
         # Or path to database file if using sqlite3.
-        'NAME': config('DB_NAME'),
+        'NAME': 'd97mijqsjdkl8g'
+        'USER': 'ienceiqnrrjcnq',
+        'PASSWORD': '780e31f7339fc569e728017fd5fc1b9739cc6feabc35f35ba108526b78df5a84',
 
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        # 'NAME': config('DB_NAME'),
+        # 'USER': config('DB_USER'),
+        # 'PASSWORD': config('DB_PASSWORD'),
 
         # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'HOST': 'central-aluminio.herokuapp.com',
+        'HOST': 'ec2-54-235-177-183.compute-1.amazonaws.com',
 
         # Set to empty string for default
         'PORT': '5432',
