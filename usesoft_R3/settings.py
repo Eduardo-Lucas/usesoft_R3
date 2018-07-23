25cb54cd61288aa732e00b5519e547f1a86d8467
Eduardo Lucas <eduardolucas40@gmail.com>
2018-07-23 20:08:18 -0300

    Final Adjustments

--- usesoft_R3/settings.py
+++ usesoft_R3/settings.py
@@ -145,7 +145,7 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
 
         # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'HOST': '',
+        'HOST': 'central-aluminio.herokuapp.com',
 
         # Set to empty string for default
         'PORT': '5432',
